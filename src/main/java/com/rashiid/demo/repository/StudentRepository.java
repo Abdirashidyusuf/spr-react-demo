59e60fe690345ee17eaf2f3884fc5f34febc46d6
Abdirashid Yusuf <abdirshidtatle@gmail.com>
2025-03-27 22:20:42 +0300

    Add [dependency-name] to pom.xml

---
+++ src/main/java/com/rashiid/demo/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.rashiid.demo.repository;
+
+import com.rashiid.demo.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByEmail(String email);
+}
