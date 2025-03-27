098f779806c4fdf78076e6a01a647dc09d8a7e13
Abdirashid Yusuf <abdirshidtatle@gmail.com>
2025-03-27 11:00:11 +0300

    Initial commit: Added backend (Spring Boot) and frontend (React) setup

---
+++ src/test/java/com/rashiid/demo/SprDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rashiid.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
