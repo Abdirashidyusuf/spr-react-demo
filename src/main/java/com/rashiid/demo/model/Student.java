ae7867a3e213c939a00663683e87b9ea3d7e8dae
Abdirashid Yusuf <abdirshidtatle@gmail.com>
2025-03-27 11:48:32 +0300

    Add Student model

---
+++ src/main/java/com/rashiid/demo/model/Student.java
@@ -0,0 +1,30 @@
+package com.rashiid.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    // Data fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_seq")
+    @SequenceGenerator(name = "student_seq", sequenceName = "student_sequence", allocationSize = 1)
+    private Long id;
+
+    private String firstName;  // Fixed typo
+    private String lastName;
+
+    @NaturalId(mutable = true)
+    private String email;
+
+    private String department;
+}
